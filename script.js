c0d28de30dadec45522e21873f0885dcfa45b426
Higor Gardin <higorgardin00@gmail.com>
2022-07-24 01:43:08 -0300

    Improved tooltip showing and cleaned code

--- script.js
+++ script.js
@@ -1,139 +1,113 @@
 // Adiciona evento de clique ao botão para selecionar texto
-document.getElementById("selectText").addEventListener("click", insertScript);
+document.getElementById('selectText').addEventListener('click', insertScript);
 
 // Injeta a função de selecionar o texto
 function insertScript() {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
-      function: selectText,
+      function: loadExtension,
     });
   });
 
+  // Fecha a janela da extensão ao carrega-la
   setTimeout(() => {
     window.close();
-  }, 500);
+  }, 100);
 }
 
 insertScript();
 
 // Executa a função para selecionar texto
-function selectText() {
+function loadExtension() {
+  /**
+   * Personaliza o cursor do mouse
+   */
+  function setCursor() {
+    const cursorPath = 'img/cursor.png';
+    const cursorUrl = chrome.runtime.getURL(cursorPath);
 
+    const elems = document.body.getElementsByTagName('*');
 
-  // const contentScriptLinks = ['libs/jquery-1.8.3.min.js'];
-
-  // contentScriptLinks.forEach(src => {
-  //   let el = document.createElement('script');
-  //   el.src = chrome.runtime.getURL(src);
-  //   document.body.appendChild(el);
-  // });
-
-
-
-
-
-  const cursorPath = 'img/cursor.png';
-  const cursorUrl = chrome.runtime.getURL(cursorPath);
-
-  const elems = document.body.getElementsByTagName("*");
-
-  for(const i of elems){
-    i.style.cursor = "url(" + cursorUrl + "), default";
+    for (const i of elems) {
+      i.style.cursor = 'url(' + cursorUrl + '), default';
+    }
   }
 
   /**
-   * 
-   * @param {Event} e 
+   * Remove o cursor do mouse
    */
-  function listener(e) {
-    e = e || window.event;
-    e.preventDefault();
-    const target = e.target || e.srcElement;
-    const text = target.textContent || target.innerText;
+  function removeCursor() {
+    const elems = document.body.getElementsByTagName('*');
 
-    console.log(target);
-    console.log(text);
+    for (const i of elems) {
+      i.style.removeProperty('cursor');
+    }
+  }
 
-
+  /**
+   * Exibe o tooltip com o texto copiado
+   * @param {number} posX Posição X do evento de clique
+   * @param {number} posY Posição Y do evento de clique
+   * @param {string} text Texto obtido do evento de clique
+   */
+  function displayTooltip(posX, posY, text) {
     var fadeDelay = 1000;
-    var fadeDuration = 1000;
+    var fadeDuration = 3000;
 
     var div = $('<div class="image-wrapper">')
       .css({
-        "left": e.pageX + 'px',
-        "top": e.pageY + 'px'
+        left: posX + 'px',
+        top: posY + 'px',
       })
-      .append($('<span>AAAA</span>'))
-      .append($('<img src="" alt="myimage" />'))
+      .append($(`<span class="tooltip">Copiado: ${text}</span>`))
       .appendTo(document.body);
-        
-    setTimeout(function() {
-      div.addClass('fade-out');			
-      setTimeout(function() { div.remove(); }, fadeDuration);
-    }, fadeDelay);
-      
 
+    setTimeout(function () {
+      div.addClass('fade-out');
+      setTimeout(function () {
+        div.remove();
+      }, fadeDuration);
+    }, fadeDelay);
   }
 
-  document.addEventListener("click",listener,false);
+  /**
+   * Registra os listeners
+   */
+  function registerListeners() {
+    /**
+     * @param {Event} e
+     */
+    function clickListener(e) {
+      e = e || window.event;
+      e.preventDefault();
+      const target = e.target || e.srcElement;
+      const text = target.textContent || target.innerText;
 
-  document.addEventListener('keydown', function(event){
-    if(event.key === "Escape"){
-      document.removeEventListener('click', listener, false);
+      console.log(target);
+      console.log(text);
 
-      const elems = document.body.getElementsByTagName("*");
+      navigator.clipboard.writeText(text);
 
-      for(const i of elems){
-        i.style.removeProperty('cursor');
+      displayTooltip(e.pageX, e.pageY, text);
+    }
+
+    /**
+     *
+     * @param {MouseEvent} event
+     */
+    function keyListener(event) {
+      if (event.key === 'Escape') {
+        removeCursor();
+        document.removeEventListener('click', clickListener, false);
+        document.removeEventListener('keydown', keyListener, false);
       }
     }
-  });
+
+    document.addEventListener('click', clickListener, false);
+    document.addEventListener('keydown', keyListener);
+  }
+
+  setCursor();
+  registerListeners();
 }
-
-// // This function selects a random topic from an array and
-// function selectText() {
-//   // This is an array to store our search terms
-//   const searchTerms = [
-//     "PC and Mobile",
-//     "Lifestyle",
-//     "Hardware",
-//     "Windows",
-//     "Mac",
-//     "Linux",
-//     "Android",
-//     "Apple",
-//     "Internet",
-//     "Security",
-//     "Programming",
-//     "Entertainment",
-//     "Productivity",
-//     "Career",
-//     "Creative",
-//     "Gaming",
-//     "Social Media",
-//     "Smart Home",
-//     "DIY",
-//     "Review",
-//   ];
-
-//   // This generates a random number between 0 and 19
-//   let selectorNumber = Math.floor(Math.random() * 20);
-
-//   // This uses the random number to select an entry from the array
-//   let selection = searchTerms[selectorNumber];
-
-//   console.log(`sdasd`);
-
-//   // // This simulates a click on the MUO website search icon
-//   // document.getElementById("js-search").click();
-//   //
-//   // // This sets the MUO website search bar as a variable
-//   // var searchBar = document.getElementById("js-search-input");
-//   //
-//   // // This inserts our random search term into the search bar
-//   // searchBar.value = searchBar.value + selection;
-//   //
-//   // // This finishes the process by activating the website form
-//   // document.getElementById("searchform2").submit();
-// }
